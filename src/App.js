ec9590d77ee534defa80c087245202d16c17159a
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-06-28 22:22:01 +0100

    using state and setSate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './components/Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <Person name="Gaiya M." age="45" />
-      <Person name="Gaiya M." age="55" />
-      <Person name="Gaiya M." age="55" >I love raping </Person>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: [
+        { name: 'Obed', age: Math.floor(Math.random() * 30) },
+        { name: 'Jude', age: Math.floor(Math.random() * 30) },
+        { name: 'Epa', age: Math.floor(Math.random() * 90) }
+      ]
+    }
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      person: [
+        { name: 'Obed M', age: Math.floor(Math.random() * 30) },
+        { name: 'Jude G', age: Math.floor(Math.random() * 30) },
+        { name: 'Epaphras', age: Math.floor(Math.random() * 90) }
+      ]
+    })
+    console.log('btn clicked');
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <br />
+        <button onClick={this.switchNameHandler} style={{ backgroundColor: 'grey', color: 'white' }} >Click me</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age} />
+        <Person name={this.state.person[1].name} age={this.state.person[1].age} />
+        <Person name={this.state.person[2].name} age={this.state.person[2].age} >I love raping </Person>
+      </div>
+    );
+  }
 }
 
 export default App;
