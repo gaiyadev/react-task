77a2dec2f99b181f76d08c37c6f4ee8e3ad9ebd0
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-06-29 00:03:43 +0100

    bug fixed

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <input type="text" value={props.name} name="name" onChange={props.changeName} />
+            <input type="text" value={props.Name} name="name" onChange={props.changeName} />
             <input type="checkbox" name="check" onClick={props.checked} />
             <p onClick={props.click} >My name is {props.name} and am from {props.age}</p>
             <p>{props.children}</p>
